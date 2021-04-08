3966d5876c8c50d219107c93ee152124e8f0b5da
0xflotus <0xflotus@gmail.com>
2021-04-08 11:44:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ function isPrivate (ip) {
 }
 
 function toNumber (ip) {
-  const [a, b, c, d] = ip.split('.').map(n => parseInt(n, 10))
+  const [a, b, c, d] = ip.split('.').map(toInt)
   return 0x1000000 * a + 0x10000 * b + 0x100 * c + d
+}
+
+function toInt (n) {
+  return parseInt(n, 10)
 }
